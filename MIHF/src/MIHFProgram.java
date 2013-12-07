6786f6e66b0924e12d13e4c9c38c62c3ac6bd0ce
EndruBoy <nyiriandris@gmail.com>
2013-12-07 22:14:35 +0100

    -lista tárgyakkal

--- MIHF/src/MIHFProgram.java
+++ MIHF/src/MIHFProgram.java
@@ -2,19 +2,21 @@ import java.io.IOException;
 
 
 public class MIHFProgram {
-
+	
 	private static ParseHTML parseHTML;
 	private static void init() {
 		parseHTML = new ParseHTML();
 	}
 	public static void main(String[] args) {
 		init();
+		//ParseHTML parseHTML = new ParseHTML();
 		try {
 			parseHTML.parse();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println(parseHTML.mTargyak);
 
 	}
 
--- MIHF/src/ParseHTML.java
+++ MIHF/src/ParseHTML.java
@@ -1,5 +1,8 @@
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,13 +10,21 @@ import org.jsoup.select.Elements;
 
 public class ParseHTML {
 	private Document doc;
+	public List<String> mTargyak = new ArrayList<String>();
 	public void parse() throws IOException {
 		doc = Jsoup.connect("https://www.vik.bme.hu/kepzes/targyak/").get();
 		Elements subjects = doc.getElementsByAttributeValue("class", "subject_list");
 		Elements links = subjects.select("a[href]");
+		int num = 0;
 		for (Element link : links) {
+			/*
 			System.out.println("\nlink : " + link.attr("href"));
 			System.out.println("text : " + link.text());
+			*/
+			num++;
+			if(num % 2 == 0){
+				mTargyak.add(link.text());
+			}
 		}
 	}
 
