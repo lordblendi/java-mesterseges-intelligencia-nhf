9bf4fab6db34ffa93443681283fc59c754e1a042
lordblendi <lordblendi@gmail.com>
2013-12-07 22:07:27 +0100

    Window

---
+++ MIHF/src/Window.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nóra
+ * Date: 2013.12.07.
+ * Time: 22:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class Window {
+    int file = 0;
+}
